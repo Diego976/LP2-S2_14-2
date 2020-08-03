54182ad924b96a652dd7182d3eafb4437327abbf
Diego Vasquez <dialvf@hotmail.com>
2020-08-03 13:15:30 -0500

    TAREA:Proyecto 3-2

--- src/java/Modelo/Venta.java
+++ src/java/Modelo/Venta.java
@@ -10,5 +10,66 @@ package Modelo;
  * @author w10
  */
 public class Venta {
+    private int idventa;
+    private String nombre;
+    private double utilidad;
+    private String vendedor;
+    private double precio;
+
+    public Venta() {
+    }
+
+    public Venta(int idventa, String nombre, double utilidad, String vendedor, double precio) {
+        this.idventa = idventa;
+        this.nombre = nombre;
+        this.utilidad = utilidad;
+        this.vendedor = vendedor;
+        this.precio = precio;
+    }
+
+    public int getIdventa() {
+        return idventa;
+    }
+
+    public void setIdventa(int idventa) {
+        this.idventa = idventa;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getUtilidad() {
+        return utilidad;
+    }
+
+    public void setUtilidad(double utilidad) {
+        this.utilidad = utilidad;
+    }
+
+    public String getVendedor() {
+        return vendedor;
+    }
+
+    public void setVendedor(String vendedor) {
+        this.vendedor = vendedor;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    
+    
+    
+    
     
 }
